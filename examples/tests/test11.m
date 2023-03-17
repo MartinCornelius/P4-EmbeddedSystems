0f3bb9cf1a8e557146880c912cdfde8e33deebe1
thomaskettunen <49203257+thomaskettunen@users.noreply.github.com>
2023-03-17 13:29:08 +0100

    NewTesties

---
+++ examples/tests/test11.m
@@ -0,0 +1,11 @@
+// test11 declares the smallest func first in the file. It should not pass
+
+func(){
+
+}
+setup(){
+
+}
+mainloop(){
+    
+}
---
+++ examples/tests/test12.m
@@ -0,0 +1,11 @@
+// test12 declares the smallest func last in the file. It should pass
+
+setup(){
+
+}
+mainloop(){
+
+}
+func(){
+
+}
