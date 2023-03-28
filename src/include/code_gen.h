a0f7427e932830aedcd77a34b85123491105782e
Martin Cornelius <62438208+MartinCornelius@users.noreply.github.com>
2023-03-28 12:38:02 +0200

    add: types code gen

--- src/include/code_gen.h
+++ src/include/code_gen.h
@@ -16,14 +16,31 @@ void emit(const char* input, char* programString)
 
 void typeToString(char* input, int type)
 {
-    
     switch(type)
     {
         case 2:
             strcpy(input, "int8_t");
             break;
+        case 3:
+            strcpy(input, "int16_t");
+            break;
+        case 4:
+            strcpy(input, "uint8_t");
+            break;
+        case 5:
+            strcpy(input, "uint16_t");
+            break;
         case 6:
             strcpy(input, "float");
+            break;
+        case 8:
+            strcpy(input, "bool");
+            break;
+        case 9:
+            strcpy(input, "char");
+            break;
+        case 10:
+            strcpy(input, "flexint");
             break;
         default:
             printf("invalid type given, defaulting to flexint\n");
