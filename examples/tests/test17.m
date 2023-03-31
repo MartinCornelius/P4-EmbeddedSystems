31d104b22de8cf7b5b603388e7f0e0a2524fe1be
Martin Cornelius <62438208+MartinCornelius@users.noreply.github.com>
2023-03-31 11:59:12 +0200

    update: update test17

--- examples/tests/test17.m
+++ examples/tests/test17.m
@@ -6,4 +6,20 @@ setup()
 mainloop()
 {
   t = 2 + 5;
+  h = 6 - 2;
+  g = 3 * 7;
+  y = 5 / 2;
+  v <- 2 + 4;
+
+  if (2 < 8 && 2 > 1)
+  {
+    bo = 4 * 3;
+    thomas = 12 + 1;
+  }
+  else if (2 > 1) {
+    martin = 69;
+  }
+  else {
+    lucas = 96;
+  }
 }
