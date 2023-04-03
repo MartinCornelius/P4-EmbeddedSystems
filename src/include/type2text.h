48a029d53751e81cf73c191626858fcfa9f3c7ef
Martin Cornelius <62438208+MartinCornelius@users.noreply.github.com>
2023-04-03 16:07:07 +0200

    add: printType function which returns type in text from an int input

---
+++ src/include/type2text.h
@@ -0,0 +1,63 @@
+#ifndef TYPE2TEXT_H
+#define TYPE2TEXT_H
+
+char *printType(int type)
+{
+  static char result[30];
+
+  switch (type)
+  {
+  case ROOT:
+    sprintf(result, "root");
+    break;
+  case SETUP:
+    sprintf(result, "setup");
+    break;
+  case MAIN:
+    sprintf(result, "mainloop");
+    break;
+  case LINES:
+    sprintf(result, "lines");
+    break;
+  case ASSIGN:
+    sprintf(result, "assign");
+    break;
+
+  /* Arithmetic operations */
+  case PLUS:
+    sprintf(result, "plus");
+    break;
+  case MINUS:
+    sprintf(result, "minus");
+    break;
+  case TIMES:
+    sprintf(result, "times");
+    break;
+  case DIV:
+    sprintf(result, "div");
+    break;
+
+  /* Types */
+  case ID:
+    sprintf(result, "identifier");
+    break;
+  case VAL:
+    sprintf(result, "integer");
+    break;
+  case VALF:
+    sprintf(result, "float");
+    break;
+
+  /* Misc */
+  case EMPTY:
+    sprintf(result, "empty");
+    break;
+  default:
+    sprintf(result, "not implemented");
+    break;
+  }
+
+  return result;
+}
+
+#endif
