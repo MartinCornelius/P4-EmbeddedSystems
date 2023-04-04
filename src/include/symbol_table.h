0348a47b60661391addc5ba6a774ab4ab2fc5076
lucas <lucasfrendorf@gmail.com>
2023-04-04 15:21:51 +0200

    Cleanup

--- src/include/symbol_table.h
+++ src/include/symbol_table.h
@@ -36,18 +36,21 @@ item* createItem(char* key, enum types value)
     return i;
 }
 
+// Hashes each character in the key
 int hashing(HashTable* table, char* key, int i)
 {
     int hash = 537;
     while (key[i] != '\0')
     {
         hash = (hash + key[i] + i) % table->size;
+        printf("Hash: %d, Key: %c, i: %d\n", hash, key[i], i);
         i++;
     }
 
     return hash;
 }
 
+// These function make double hashing easier to implement
 int hash(HashTable* table, char* key)
 {
     return hashing(table, key, 0);
@@ -58,7 +61,7 @@ int doubleHash(HashTable* table, char* key, int i)
     return hashing(table, key, i);
 }
 
-void insert(HashTable* table, char* key, enum types value)
+void insertSymbol(HashTable* table, char* key, enum types value)
 {
     item* hItem = createItem(key, value);
     int index = hash(table, key);
@@ -91,6 +94,7 @@ enum types searchSymbol(HashTable* table, char* key)
         return not_found_enum;
     }
 
+    // Double hashing until symbol is found or empty slot is found
     while (current != NULL && strcmp(current->key, key) != 0)
     {
         printf("%s: Search collision at index %d, trying i%d, double hashing\n", key, index, i+1);
@@ -108,7 +112,7 @@ enum types searchSymbol(HashTable* table, char* key)
     return current->value;
 }
 
-void createSymbol(HashTable* table, int type, char* name)
+void createSymbol(HashTable* table, char* key, int value)
 {
     if (table == NULL)
     {
@@ -117,10 +121,10 @@ void createSymbol(HashTable* table, int type, char* name)
         return;
     }
 
-    if (searchSymbol(table, name) == not_found_enum)
+    if (searchSymbol(table, key) == not_found_enum)
     {
-        printf("%s: Symbol created\n", name);
-        insert(table, name, (enum types)type);
+        printf("%s: Symbol created\n", key);
+        insertSymbol(table, key, (enum types) value);
     }
     else
     {
@@ -133,6 +137,7 @@ void printTable(HashTable* table)
     for (int i = 0; i < table->size; i++)
     {
         item* current = table->items[i];
+
         if (current != NULL)
         {
             printf("___________________\n");
