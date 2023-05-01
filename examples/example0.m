8c4265740edde1731661c8fd1698e2066cf1cebe
Martin Cornelius <62438208+MartinCornelius@users.noreply.github.com>
2023-05-01 23:35:28 +0200

    fix codegen

--- examples/example0.m
+++ examples/example0.m
@@ -5,15 +5,14 @@ setup()
 mainloop()
 {
   int8 a = 12;
-  int16 b = 55;
-  pepsiMax(a -> b);
+  int8 b = 55;
+  redbull(a -> b);
+  print(b);
 }
 
 func redbull(int8 a, int8 b)
 {
-}
-
-func pepsiMax(int8 a -> int16 b)
-{
-  redbull(10, 5);
+    print(a);
+    a = 2 + a;
+    print(a);
 }
