cfd2c3d0b21be0351014dad7ad4350d21c471160
Hugin <huginj@gmail.com>
2023-05-10 08:34:09 +0200

    Minor appeareance fix

--- examples/example0.m
+++ examples/example0.m
@@ -4,14 +4,16 @@ setup()
 }
 mainloop(){
   uint16 a = 2;
-  uint16 c = 3;
-  while (a > 10)
+  uint16 b = 2;
+  uint16 c = 2;
+  uint16 d = 2;
+  uint16 i = 0;
+  while (i <= 10)
   {
-    uint16 b = 5 + a;
-    while (a < 5)
-    {
-      c = a + 2;
-    }
+    a = b + c;
+    d = a * i;
+    i = i + 1;
+    
   }
   print(c);
 }
